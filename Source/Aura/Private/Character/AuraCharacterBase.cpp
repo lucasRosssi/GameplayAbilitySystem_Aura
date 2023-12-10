154f0eccf85a703b54c6cbaa46702a40829a6765
Lucas Rossi <lucasrm2305@gmail.com>
2023-12-10 14:03:54 -0300

    work on secondary attributes

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -29,16 +29,25 @@ void AAuraCharacterBase::InitAbilityActorInfo()
 {
 }
 
-void AAuraCharacterBase::InitializePrimaryAttributes() const
+void AAuraCharacterBase::ApplyEffectToSelf(
+	TSubclassOf<UGameplayEffect> GameplayEffectClass,
+	float Level
+) const
 {
 	UAbilitySystemComponent* ASC = GetAbilitySystemComponent();
 	check(IsValid(ASC));
-	check(DefaultPrimaryAttributes);
+	check(GameplayEffectClass);
 	
 	const FGameplayEffectContextHandle ContextHandle = ASC->MakeEffectContext();
 	const FGameplayEffectSpecHandle SpecHandle = ASC->MakeOutgoingSpec(
-		DefaultPrimaryAttributes,
-	 1.f, 
+		GameplayEffectClass,
+	 Level, 
 	ContextHandle);
 	ASC->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), ASC);
+}
+
+void AAuraCharacterBase::InitializeDefaultAttributes() const
+{
+	ApplyEffectToSelf(DefaultPrimaryAttributes, 1.f);
+	ApplyEffectToSelf(DefaultSecondaryAttributes, 1.f);
 }
